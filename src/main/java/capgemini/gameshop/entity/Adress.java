711dd678e91ecfd18941ec763703a8c99650fbde
ohanke <steamoha@wp.pl>
2022-06-11 12:45:36 +0200

    Added project lombok. Closes #1.

--- src/main/java/capgemini/gameshop/entity/Adress.java
+++ src/main/java/capgemini/gameshop/entity/Adress.java
@@ -1,5 +1,14 @@
 package capgemini.gameshop.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Adress {
     private String country;
     private String address;
@@ -7,54 +16,4 @@ public class Adress {
     private String city;
     private String zip;
 
-    public Adress(String country, String address, String state, String city, String zip) {
-        this.country = country;
-        this.address = address;
-        this.state = state;
-        this.city = city;
-        this.zip = zip;
-    }
-
-    public Adress() {
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
 }
