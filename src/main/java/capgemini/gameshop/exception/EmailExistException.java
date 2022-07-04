1010a79a22cb594fb407e6c98042dd7cfa788381
michaltworuszka <michal.tworuszka@gmail.com>
2022-07-04 12:01:21 +0200

    UserRestControllerIntegrationTest.java Integration tests, and feedback change to UserRestControllerUnitTest.java

--- src/main/java/capgemini/gameshop/exception/EmailExistException.java
+++ src/main/java/capgemini/gameshop/exception/EmailExistException.java
@@ -1,9 +1,5 @@
 package capgemini.gameshop.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.FORBIDDEN)
 public class EmailExistException extends RuntimeException {
 
 
